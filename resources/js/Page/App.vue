07f3f7ebc005430542dd242053d9d53508c5374f
YellowBearPooh <yellowbearpooh0110@gmail.com>
2022-07-27 02:45:57 +0900

    chat complete

--- resources/js/Page/App.vue
+++ resources/js/Page/App.vue
@@ -6,13 +6,14 @@
             Please enter a message that will be sent to the clients (this web
             application and android application) via Laravel Echo.
         </p>
-        <form id="newMessageForm" @submit="increment">
+        <form id="newMessageForm" @submit="onSubmit">
             <div class="form-group">
                 <label for="messageInput">Message</label>
                 <input
                     type="text"
                     class="form-control"
                     id="messageInput"
+                    name="message"
                     placeholder="Enter some message"
                 />
             </div>
@@ -22,7 +23,9 @@
             <br /><br />
             <div class="form-group">
                 <label for="messagesList">Received messages:</label>
-                <ul id="messagesList" class="list-group"></ul>
+                <ul class="list-group">
+                    <li v-for="message in messages">{{ message }}</li>
+                </ul>
             </div>
         </form>
     </main>
@@ -32,19 +35,23 @@
 export default {
     data() {
         return {
-            names: [],
+            messages: [],
         };
     },
     mounted() {
         let that = this;
         Echo.channel("message").listen("MessageCreated", (data) => {
-            that.names.push(data.message);
+            that.messages.push(data.message);
         });
     },
     methods: {
-        increment(event) {
+        onSubmit(event) {
             event.preventDefault();
-            fetch("/api/messages");
+            const _message = event.currentTarget.message.value;
+            if (!_message) return;
+            const _data = new URLSearchParams();
+            _data.append("message", _message);
+            fetch("/api/message", { method: "POST", body: _data });
         },
     },
 };
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
-
-Route::get('/t', function () {
-    event(new \App\Events\MessageCreated('Test'));
-    dd('Event Run Successfully.');
-});
